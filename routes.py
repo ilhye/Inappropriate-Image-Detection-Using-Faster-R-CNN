37eef2bf54f643897992e40f443407022860fb82
ilhye <144883820+ilhye@users.noreply.github.com>
2025-07-02 00:15:28 +0800

    Add auth

--- routes.py
+++ routes.py
@@ -1,9 +1,10 @@
 import os
 
-from flask import Blueprint, render_template, redirect, url_for, flash, request
+from flask import Blueprint, render_template, redirect, session, url_for, flash, request
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed, FileRequired
-from wtforms import SubmitField
+from wtforms import SubmitField, StringField, PasswordField, BooleanField
+from wtforms.validators import DataRequired
 from werkzeug.utils import secure_filename
 from PIL import Image
 from frcnn import draw_boxes, contains_label
@@ -16,40 +17,78 @@ ANNOT_FOLDER = os.path.join("static", "annotated")
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 os.makedirs(ANNOT_FOLDER, exist_ok=True)
 
-# Form
+# Create Post
 class CreatePost(FlaskForm):
     uploadImg = FileField(
-        "Upload Image",
-        validators=[FileRequired(), FileAllowed(["jpg", "jpeg", "png"], "Images only!")],
+        "Upload File",
+        validators=[FileRequired("Please enter file"), FileAllowed(["jpg", "jpeg", "png"], "Images only!")],
     )
     submit = SubmitField("Post")
 
+# Login Form
+class Login(FlaskForm):
+    username = StringField('Username', validators=[
+                           DataRequired("Please enter your username")])
+    password = PasswordField('Password', validators=[
+                             DataRequired("Please enter your password")])
+    submit = SubmitField('Sign In')
+
+class NewAccount(FlaskForm):
+    username = StringField('Username', validators=[
+                           DataRequired("Please enter your username")])
+    password = PasswordField('Password', validators=[
+                             DataRequired("Please enter your password")])
+    submit = SubmitField('Sign In')
+
 # Route
-@bp.route("/", methods=["GET", "POST"])
+@bp.route("/post", methods=["GET", "POST"])
 def index():
+    username = session.get("username")
     form = CreatePost()
     if form.validate_on_submit():
         file = request.files.get("uploadImg")
         pil_img = Image.open(file.stream).convert("RGB")
 
-        # Has person == not save to upload
+        # Has violence == not save to upload
         if contains_label(pil_img, "violence", score_thresh=0.8):
             flash("Upload rejected: Image contains violence")
             return redirect(url_for("routes.index"))
         
         result_img = draw_boxes(pil_img.copy())
 
-        # No person == annotate and save
+        # No violence == annotate and save
         filename = secure_filename(file.filename)
         save_path = os.path.join(ANNOT_FOLDER, f"annot_{filename}")
         result_img.save(save_path)
 
-        return redirect(url_for("routes.result", filename=f"annot_{filename}"))
+        return redirect(url_for("routes.result", filename=f"annot_{filename}", username=username))
 
-    return render_template("index.html", form=form)
+    return render_template("index.html", form=form, username=username)
 
 # Route: Result
 @bp.route("/result/<path:filename>")
 def result(filename):
     image_url = url_for("static", filename=f"annotated/{filename}")
     return render_template("result.html", image_url=image_url)
+
+@bp.route("/", methods=["GET", "POST"])
+def login():
+    form = Login()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        session["username"] = username
+        return redirect(url_for("routes.index"))
+    return render_template("signIn.html", form=form)
+
+@bp.route("/signUp", methods=["GET", "POST"])
+def signUp():
+    form = NewAccount()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        session["username"] = username
+        return redirect(url_for("routes.index"))
+    return render_template("signUp.html", form=form)
